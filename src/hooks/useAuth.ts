167251c2861ae978f639d07409f6131da1c9ee70
tutu-tonton <50165379+tutu-tonton@users.noreply.github.com>
2021-05-22 07:52:46 +0900

    メッセージ表示機能実装 ログイン成功・失敗時のメッセージがToastで出てくる。カスタムフック作成

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,31 +1,37 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
+
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
   const login = useCallback(
     (id: string) => {
       setLoading(true);
-      console.log(id);
 
       axios
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
             alert("nothing found");
           }
         })
-        .catch(() => alert("ログインできません"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [history]
+    [history, showMessage]
   );
   return { login };
 };
