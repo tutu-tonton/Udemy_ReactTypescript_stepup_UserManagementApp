a849615f3dc69bdafb3b079de0e642dc4436a7d8
tutu-tonton <50165379+tutu-tonton@users.noreply.github.com>
2021-05-22 09:09:03 +0900

    ユーザーカード作成 ユーザーカードコンポーネント作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>UserManagementページ</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard imageUrl="url" userName="hoge" fullName="hogehoge" />
+      </WrapItem>
+    </Wrap>
+  );
 });
